5d97297c44745c38f78a5d057311d825f09469f8
ALBERT CATALAN TATJER <acatalta11.alumnes@ub052229.mat.ub.edu>
2020-02-19 18:03:35 +0100

    p1

---
+++ src/main/java/stream_utils/Datagram.java
@@ -0,0 +1,13 @@
+ package stream_utils;
+
+ public class Datagram{
+     
+     ComUtils comStream;
+
+     public Datagram(ComUtils comStream){
+         this.comStream = comStream;
+     }
+
+     public void
+
+ }
